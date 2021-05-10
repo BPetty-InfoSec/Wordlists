3ec061bcf3168e24dc4e7bdf5b7b37922e238595
BPetty-InfoSec <bwpetty@protonmail.com>
2021-05-10 18:56:12 -0400

    add basic name collision resolution

--- buildlist.py
+++ buildlist.py
@@ -16,9 +16,9 @@ def main():
     #print(fileList)
     for file in fileList:
         #print(fileList[file] + " : ./Wordlists/" + file)
-        try:
+        try:                                #Create symlinks in ./Wordlists
             os.symlink("." + fileList[file], "./Wordlists/" + file)
-        except:
+        except:                             #Create ./Wordlists if it doesn't exist
             os.mkdir("Wordlists")
             os.symlink("." + fileList[file], "./Wordlists/" + file)
 
@@ -26,17 +26,25 @@ def checkDir(path):
     returnList = {}
     #print(path)
     for file in os.listdir(path):
-        if file == ".git":
+        if file == ".git":                  #Skip the .git directory in repos
             continue
-        elif os.path.isdir(path + "/" + file):
+        elif os.path.isdir(path + "/" + file):  #If this is a directory recursively call function
             #print(path + " : " + file)
             tempReturn = checkDir(path + "/" + file)
-            for item in tempReturn:
+            for item in tempReturn:         #Process recursively processed files into returnList
                 try:
-                    returnList[item] = tempReturn[item]
+                    if checkName(item,returnList):  #Check if name exists
+                        i = 0               #Loop iteration counter
+                        while not checkName(item,returnList):
+                            item = iterName(item)   #Find a name that will work
+                            i += 1
+                        else:
+                            returnList[item] = tempReturn[item]
+                    else:
+                        returnList[item] = tempReturn[item] #Add file and path to returnList
                 except:
-                    returnList[item + "1"] = tempReturn[item]
-        else:
+                    returnList["2" + item] = tempReturn[item]   #Avoid edge case with identical filenames
+        else:                               #If this is a file, process the file
             if file == "CREDIT.txt":        #Skip CREDIT.txt files
                 continue
             elif file == "README.md":       #Skip README files
@@ -45,7 +53,20 @@ def checkDir(path):
                 return []
             else:
                 #print(file + " is a file")
-                returnList[file] = path + "/" + file
+                returnList[file] = path + "/" + file    #Add the filename and path to the returnList dictionary
     return returnList
+
+def iterName(iterName, curNum):             #Increase the number after a filename for duplicate files
+    nameString = iterName.split('.')        #Split the string at "."
+    nameString[0] = nameString[0] + str(int(curNum) + 1)
+    for part in nameString:                 #Reconstruct string, even if multiple "."
+        returnName = nameString[part]
+    return returnName
+
+def checkName(checkName, checkList):        # Check to see if a name is already in the list
+    if checkName in checkList:
+        return False
+    else:
+        return True
 
 main()
