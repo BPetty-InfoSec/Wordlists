370f9596b3e717b1bcee59d5612577ea6cc26eeb
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-02-27 19:40:42 -0500

    main menu left column and layout
    This initiates the layout for the main menu (2 columns, 49 characters wide, with left/right bounding box, and a separator in the middle) along with a title row that contains a 21-character wide (centered) menu title. Currently, the left (only) column accepts a List input, which is composed of 2-item lists making up a number and a display name
    
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

--- bin/mainmenu.py
+++ bin/mainmenu.py
@@ -1,2 +1,19 @@
+def PrintHeader(title="Main Menu"):
+    print("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- " + title.center(21, " ") + " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+")
+
+def PrintLine(optionL, optionR):
+    leftOption = optionL[0] + "  " + optionL[1]
+    # rightOption = optionR[0] + "  " + optionR[1] 
+    rightOption = ""
+    print("|  " + leftOption.ljust(49, " ") + " | " + rightOption.ljust(49, " ") + "  |")
+
+def DisplayOptions(optionsL, optionsR):
+    if len(optionsL) > len(optionsR):
+        for item in optionsL:
+            PrintLine(item,"")
+
 def DisplayMenu():
-    print("Menu test")
+    PrintHeader()
+    menuOptionsL = [["1.","Show Lists"], ["2.","Build Lists"],["3.","Categories"]]
+    menuOptionsR = [["X.", "Exit"],["W.","Wipe Database"]]
+    DisplayOptions(menuOptionsL, menuOptionsR)
