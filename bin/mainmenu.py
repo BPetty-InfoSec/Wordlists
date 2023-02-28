e4e7eab7357d2022ac59f199a32b382b0787bd68
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-02-27 21:21:16 -0500

    update menu to show 2nd column safely
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

--- bin/mainmenu.py
+++ bin/mainmenu.py
@@ -1,19 +1,47 @@
+from os import system, name
+
 def PrintHeader(title="Main Menu"):
-    print("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- " + title.center(21, " ") + " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+")
+    print("+" + "-=" * 38 + "-+")
+    print("| " + title.center(75," ") + " |")
+    print("+" + "-" * 77 + "+")
 
 def PrintLine(optionL, optionR):
     leftOption = str(optionL[0]) + ".  " + optionL[1]
-    # rightOption = optionR[0] + "  " + optionR[1] 
-    rightOption = ""
-    print("|  " + leftOption.ljust(49, " ") + " | " + rightOption.ljust(49, " ") + "  |")
+    if optionR:
+        rightOption = str(optionR[0]) + "  " + optionR[1] 
+    else:
+        rightOption = ""
+    print("|  " + leftOption.ljust(35, " ") + " | " + rightOption.ljust(35, " ") + "  |")
+    
+def PrintFooter():
+    print("+" + "-" * 77 + "+")
 
 def DisplayOptions(optionsL, optionsR):
-    if len(optionsL) > len(optionsR):
-        for item in optionsL:
-            PrintLine(item,"")
+    lenL = len(optionsL)
+    lenR = len(optionsR)
+    if lenL > lenR:
+        for index in range(len(optionsL)):
+            if index < lenR:
+                PrintLine(optionsL[index], optionsR[index])
+            else:
+                PrintLine(optionsL[index], "")
+    else:
+        for index in range(len(optionsR)):
+            if index < lenL:
+                PrintLine(optionsL[index], optionsR[index])
+            else:
+                PrintLine("", optionsR[index])
+            
+def ClearScreen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 def DisplayMenu():
+    ClearScreen()
     PrintHeader()
     menuOptionsL = [[1,"Show Lists"], [2,"Build Lists"],[3,"Categories"]]
     menuOptionsR = [["X.", "Exit"],["W.","Wipe Database"]]
     DisplayOptions(menuOptionsL, menuOptionsR)
+    PrintFooter()
