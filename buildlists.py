900bf7d708e78493eb967d64a8bea7bd5ccef73d
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-02-27 19:09:25 -0500

    connect to db
    Create database connection, and database if it does not already exist. Also remove db from Git, as it will be created on first run, and this way it will not have data from my local build carried over to everyone.
    
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

--- buildlists.py
+++ buildlists.py
@@ -1,10 +1,25 @@
 import bin.mainmenu as menu
+from contextlib import closing
 import sqlite3
 
-con = sqlite3.connect("bin/wordlists.db")
-cur = con.cursor()
+# Create database tables
+def CreateDBTables():
+    with closing(sqlite3.connect("bin/wordlists.db")) as con:
+        with closing(con.cursor()) as cur:
+            listsTable = "CREATE TABLE lists (id, name, categories, filePath)"
+            categoriesTable = "CREATE TABLE categories (id, name)"
+            try:
+                cur.execute(listsTable)
+            except sqlite3.OperationalError:
+                print("Main table already exists.")
+            
+            try:
+                cur.execute(categoriesTable)
+            except sqlite3.OperationalError:
+                print("Categories table already exists.")
 
 def main():
+    CreateDBTables()
     menu.DisplayMenu()
 
 main()
