efe857d1dc5b0b254c1a6e954a6688ca53977992
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-02-27 21:27:45 -0500

    Squashed commit of the following:
    commit 7ebd1039b0ca44d4004dece028029a0919eb6190
    Author: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    Date:   Mon Feb 27 21:23:11 2023 -0500
    
        move main DisplayMenu to top of file for ease of editing
    
        Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    
    commit e4e7eab7357d2022ac59f199a32b382b0787bd68
    Author: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    Date:   Mon Feb 27 21:21:16 2023 -0500
    
        update menu to show 2nd column safely
    
        Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    
    commit 0ecd0efede5306abeb41246e73113d9381cf8fd9
    Author: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    Date:   Mon Feb 27 19:41:09 2023 -0500
    
        fix to include item number in list as int instead of string
    
        Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    
    commit 370f9596b3e717b1bcee59d5612577ea6cc26eeb
    Author: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    Date:   Mon Feb 27 19:40:42 2023 -0500
    
        main menu left column and layout
    
        This initiates the layout for the main menu (2 columns, 49 characters wide, with left/right bounding box, and a separator in the middle) along with a title row that contains a 21-character wide (centered) menu title. Currently, the left (only) column accepts a List input, which is composed of 2-item lists making up a number and a display name
    
        Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
    
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

--- bin/mainmenu.py
+++ bin/mainmenu.py
@@ -1,2 +1,48 @@
+from os import system, name
+
+
 def DisplayMenu():
-    print("Menu test")
+    ClearScreen()
+    PrintHeader()
+    menuOptionsL = [[1, "Show Lists"], [2, "Build Lists"], [3, "Categories"]]
+    menuOptionsR = [["X.", "Exit"], ["W.", "Wipe Database"]]
+    DisplayOptions(menuOptionsL, menuOptionsR)
+    PrintFooter()
+    
+def PrintHeader(title="Main Menu"):
+    print("+" + "-=" * 38 + "-+")
+    print("| " + title.center(75," ") + " |")
+    print("+" + "-" * 77 + "+")
+
+def PrintLine(optionL, optionR):
+    leftOption = str(optionL[0]) + ".  " + optionL[1]
+    if optionR:
+        rightOption = str(optionR[0]) + "  " + optionR[1] 
+    else:
+        rightOption = ""
+    print("|  " + leftOption.ljust(35, " ") + " | " + rightOption.ljust(35, " ") + "  |")
+    
+def PrintFooter():
+    print("+" + "-" * 77 + "+")
+
+def DisplayOptions(optionsL, optionsR):
+    lenL = len(optionsL)
+    lenR = len(optionsR)
+    if lenL > lenR:
+        for index in range(len(optionsL)):
+            if index < lenR:
+                PrintLine(optionsL[index], optionsR[index])
+            else:
+                PrintLine(optionsL[index], "")
+    else:
+        for index in range(len(optionsR)):
+            if index < lenL:
+                PrintLine(optionsL[index], optionsR[index])
+            else:
+                PrintLine("", optionsR[index])
+            
+def ClearScreen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
