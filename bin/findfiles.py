7c102402be5c3ef950240b2ef49e35cd875e0d4a
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-02-28 22:22:33 -0500

    add module to find files
    Added the module to find files recursively in the lib directory. Also added lib/ to .gitignore, as it will contain wordlists which are also repos, rather than code.
    
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

---
+++ bin/findfiles.py
@@ -0,0 +1,17 @@
+import os
+
+def GetFilenames(pathVar):
+    print("Path: " + pathVar)
+    listing = os.listdir(pathVar)
+    filesList = []
+    dirsList = []
+    for item in listing:
+        if os.path.isfile(pathVar + "/" + item):
+            filesList.append(pathVar + "/" + item)
+        else:
+            dirsList.append(pathVar + "/" + item)
+    for dir in dirsList:
+        addList = GetFilenames(dir)
+        for item in addList:
+            filesList.append(item)
+    return filesList
